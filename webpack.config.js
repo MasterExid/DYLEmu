8e7c6467dfd00cd8d13d1b557b4eb80ea4372875
MasterExid <149447521+MasterExid@users.noreply.github.com>
2024-05-20 07:15:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'tinySheet.min.js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    path: path.resolve(__dirname, 'dist')
+  },
+};
